c1ae6cf5189ce00d4638621ac515f517a279daee
Simon <sa.schroffner@gmail.com>
2018-10-07 19:16:17 +0200

    Added Angular Testing Recipes

---
+++ angular-testing-recipes/src/app/components/dynamic-styles.component.spec.ts
@@ -0,0 +1,33 @@
+import {ComponentFixture, TestBed, async} from '@angular/core/testing';
+import {DynamicStylesComponent} from './dynamic-styles.component';
+import {customMatchers} from "../utils/custom-matchers";
+import * as custom from "../utils/custom-matchers";
+
+
+describe('DynamicStylesComponent', () => {
+    let component: DynamicStylesComponent;
+    let fixture: ComponentFixture<DynamicStylesComponent>;
+
+    beforeEach(async(() => {
+        jasmine.addMatchers(customMatchers);
+        TestBed.configureTestingModule({
+            declarations: [DynamicStylesComponent]
+        });
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DynamicStylesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should correctly set the background style', () => {
+        custom.expect(fixture.debugElement.children[0].nativeElement).toHaveStyle({
+            'background-color': 'black'
+        });
+    });
+});
